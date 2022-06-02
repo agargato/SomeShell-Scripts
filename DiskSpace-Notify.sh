f753a45f35be8c8d471250e66dab04cb7cdd7edd
Rafael Agar <94949315+agargato@users.noreply.github.com>
2022-06-02 15:53:58 -0400

    DiskSpace-Notify

---
+++ DiskSpace-Notify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#50 is a reference
+
+a=`df -h | egrep -v "tmpfs|devtmpfs" | tail -n+2 | awk '{print $5}' | cut -d'%' -f1`
+
+for i in $a
+do
+        if [ $i -ge 50 ]
+        then
+        echo Check disk space $i `df -h | grep $i`
+        fi
+done
